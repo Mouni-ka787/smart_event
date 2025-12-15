3cea3bb021e2c5c8493095e84c3a93cff3527931
Mounika <mounika2004117@gmail.com>
2025-12-15 13:47:06 +0530

    Fix: remove distDir out for Vercel deploy

--- smarteventx-v2/next.config.js
+++ smarteventx-v2/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove output: 'export' since we have dynamic routes that require runtime data fetching
-  // output: 'export',
-  distDir: 'out',
   images: {
     unoptimized: true
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
